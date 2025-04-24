28f92256eafe710edeeb93614369cf9fa5fb17a5
JLOlieira <lukaspkmn@gmail.com>
2025-04-24 13:23:55 -0300

    ready_to_deploy

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const projects = document.querySelector('section#projects');
 const about = document.querySelector('section#about');
 const main = document.querySelector('main');
 
+let details_btn = document.querySelectorAll('.details');
+const project_details = document.querySelector('.project-details');
+
+details_btn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        project_details.classList.add('active');
+        window.scrollTo(0, 0);
+    });
+});
+project_details.addEventListener('click', (e) => {
+    
+    if (e.target.classList.contains('project-details')) {
+        project_details.classList.add('closing');
+        setTimeout(() => {
+            project_details.classList.remove('active');
+            project_details.classList.remove('closing');
+        }, 450);
+    }
+});
+
 sectionLinks.forEach(section => {
     section.addEventListener('click', (e) => {
         let tabs = document.querySelectorAll('.section-link');
